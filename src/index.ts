6364d936910c9283efe5d836cf684d32347069b2
Jason Miller <contact@jasonmiller.nl>
2021-12-06 20:07:52 +0100

    fix: add index file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import GlobeScene from "./components/globe";
+import Arc from "./components/arc";
+import Marker from "./components/marker";
+import Bar from "./components/bar";
+import Globe from "./components/globe";
+
+export { GlobeScene, Globe, Arc, Bar, Marker }
