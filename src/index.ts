da6d61c50aaa5f296ac849b6c18dac0fc3149685
Jason Miller <contact@jasonmiller.nl>
2021-12-06 20:39:45 +0100

    fix: remove redundant import
    Co-authored-by: Wouter <wouter@thisisnota.studio>

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import GlobeScene from "./components/globe";
 import Arc from "./components/arc";
 import Marker from "./components/marker";
 import Bar from "./components/bar";
-import Globe from "./components/globe";
 
 export { GlobeScene, Globe, Arc, Bar, Marker }
