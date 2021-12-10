5306ff35e9bc7b3eefc64e915577aa69c20a75cc
Wouter <wouter@thisisnota.studio>
2021-12-10 15:30:39 +0100

    fix: type fixes

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,10 @@ import Marker from './components/marker';
 import Bar from './components/bar';
 
 import { GeoJSONType } from './types/geojson';
-import { GlobeConfig } from './types/globe';
 import { MarkerConfig } from './types/marker';
 import { BarConfig } from './types/bar';
 import { ArcConfig } from './types/arc';
 
 export {
-  GlobeScene, Arc, Bar, Marker, GeoJSONType, GlobeConfig, MarkerConfig, ArcConfig, BarConfig,
+  GlobeScene, Arc, Bar, Marker, GeoJSONType, MarkerConfig, ArcConfig, BarConfig,
 };
