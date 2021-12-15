b98ee7c77e2f23d69d9aaf83281a3694e7e3f90d
Wouter <wouter@thisisnota.studio>
2021-12-15 10:32:21 +0100

    fix: small shader optimization

--- src/shaders/atmosphere.frag.glsl
+++ src/shaders/atmosphere.frag.glsl
@@ -5,7 +5,7 @@ void main() {
     float atmoFade = dot(vertexNormal, vec3(0.0, 0.0, 1.0));
 
     float upperAtmosphere = pow(0.75 - atmoFade, 2.0) / 2.0;
-    float lowerAtmosphere = smoothstep(0.0, 1.0, pow(0.82 - atmoFade , 48.0) / 150.0);
+    float lowerAtmosphere = smoothstep(0.0, 1.0, pow(0.82 - atmoFade , 48.0) / 120.0);
     float atmosphere = lowerAtmosphere + upperAtmosphere;
 
     float edgeFade = dot(vertexNormal.xy, vec2(-1.0, 1.0)) + 0.25;
